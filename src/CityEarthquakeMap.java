ea079b619c6286d96b0a4dad98bf2a7620492e75
ahdieh emadi <ahdieh_e@yahoo.com>
2020-03-04 13:55:37 -0500

    create a list of PointFeature objects and Marker objects to create multiple markers on the map

--- src/CityEarthquakeMap.java
+++ src/CityEarthquakeMap.java
@@ -32,28 +32,46 @@ public class CityEarthquakeMap extends PApplet{
 		
 		// Valdivia, Chile
 		Location valLoc = new Location(-38.14f, -73.03f);
-		Feature valEq = new PointFeature(valLoc);
+//		Feature valEq = new PointFeature(valLoc);
+		PointFeature valEq = new PointFeature(valLoc);
 		valEq.addProperty("title", "Valdivia, Chile");
 		valEq.addProperty("magnitude", "9.5");
 		valEq.addProperty("date", "May 22, 1960");		
 		
-		// adding a marker on Valdivia		
+/*		// adding a marker on Valdivia		
 		Marker valMk = new SimplePointMarker(valLoc, valEq.getProperties());
 		valMk.setColor(color(255,0,0));
-		map.addMarker(valMk);
+		map.addMarker(valMk);*/
 		
 		
 		// Alaska
 		Location alaskaLoc = new Location(61.02, -147.65);
-		Feature alaskaEq = new PointFeature(alaskaLoc);
+//		Feature alaskaEq = new PointFeature(alaskaLoc);
+		PointFeature alaskaEq = new PointFeature(alaskaLoc);		
 		alaskaEq.addProperty("title", "Alaska");
 		alaskaEq.addProperty("magnitude", "9.2");
 		alaskaEq.addProperty("date", "April 28, 1964");
 		
-		// adding a marker on Alaska		
+/*		// adding a marker on Alaska		
 		Marker alaskaMk = new SimplePointMarker(alaskaLoc, alaskaEq.getProperties());
 		alaskaMk.setColor(color(0,255,0));
-		map.addMarker(alaskaMk);
+		map.addMarker(alaskaMk);*/
+		
+		
+		// adding a list of PointFeature objects to store multiple Feature objects (multiple locations)
+		List<PointFeature> bigEqs = new ArrayList<PointFeature>();		
+		bigEqs.add(valEq);
+		bigEqs.add(alaskaEq);
+		
+		// adding a list to store markers
+		List<Marker> markers = new ArrayList<Marker>();
+		for (PointFeature eq: bigEqs) {
+			Marker marker = new SimplePointMarker(eq.getLocation(), eq.getProperties());
+			marker.setColor(color(255,0,0));
+			markers.add(marker);
+		}
+		
+		map.addMarkers(markers);
 		
 	}
 	
